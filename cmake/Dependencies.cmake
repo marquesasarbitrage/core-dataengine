550fa49c783da8511f4ecd48c37b01c18ec317fb
vannavolgaa <marquesasaribitrage@gmail.com>
2025-11-11 16:13:15 +0100

    correction

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -6,7 +6,7 @@ FetchContent_MakeAvailable(json)
 FetchContent_Declare(pybind11 URL https://github.com/pybind/pybind11/archive/refs/tags/v3.0.1.tar.gz)
 FetchContent_MakeAvailable(pybind11)
 
-FetchContent_Declare(curl URL https://curl.se/download/curl-8.7.0.tar.gz)
+FetchContent_Declare(curl URL https://curl.se/download/curl-8.5.0.tar.gz)
 FetchContent_MakeAvailable(curl)
 
 FetchContent_Declare(mongocxx URL https://github.com/mongodb/mongo-cxx-driver/releases/download/r4.1.2/mongo-cxx-driver-r4.1.2.tar.gz)
